a4c3e5b8add12d7b32e62e31121c25937e5d16b2
Stefan Niemeyer <stefan.niemeyer@vkb.de>
2026-01-02 18:12:28 +0100

    install Longhorn

--- vm-scripts/initialize.sh
+++ vm-scripts/initialize.sh
@@ -4,16 +4,16 @@
 sed --in-place 's/^#?PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
 systemctl reload ssh
 
-# Install K3
+echo "Install K3"
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --cluster-init --write-kubeconfig-mode=644 --disable=traefik" sh
 sleep 20
 
-# Install Helm
+echo "Install Helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-# Install Traefik
+echo "Install Traefik"
 cat > traefik-values.yaml <<EOF
 ## File values.yaml ##
 providers:
@@ -40,6 +40,15 @@ chown -R "${USER}:${USER}" "/${HOME}/.kube"
 kubectl create namespace traefik
 helm upgrade --install --namespace traefik traefik traefik/traefik -f traefik-values.yaml
 
-# Install K9s
+echo "Install Longhorn"
+apt update
+apt install nfs-common open-iscsi -y
+systemctl enable --now iscsid
+
+helm repo add longhorn https://charts.longhorn.io
+helm repo update
+helm install longhorn longhorn/longhorn --namespace longhorn-system --create-namespace --version 1.10.1
+
+echo "Install K9s"
 wget -q -O - https://github.com/derailed/k9s/releases/download/v0.50.16/k9s_Linux_amd64.tar.gz | tar xz -C /usr/local/bin k9s
 chown root:root /usr/local/bin/k9s
